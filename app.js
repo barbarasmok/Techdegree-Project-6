255f8c658e225360f1d435508172db416e675167
BarbaraSmok <barb@kpimedia.com>
2019-12-20 22:13:59 -0300

    set up the phrases array

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const qwerty = document.querySelector('#querty');
-const phrase = document.querySelector('#phrase');
+const phrases = document.querySelector('#phrase');
 const missed = 0;
 const startButton = document.querySelector('.btn__reset');
 const start = document.querySelector('.start');
 
+//hide the display start class once you click the start button
 startButton.addEventListener ('click', () => {
     start.style.display = 'none';
 } );
+
+//set up an array
+phrases = [
+    'I love avocado',
+    'I love dogs',
+    'Cats are awesome',
+    'Dogs are friendly',
+    'Love is everywhere'
+];
+
