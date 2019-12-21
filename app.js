f0f68bf0b786d26661c419a492709e3b260b9dee
BarbaraSmok <barb@kpimedia.com>
2019-12-20 21:59:08 -0300

    set up the variables

---
+++ app.js
@@ -0,0 +1,10 @@
+const qwerty = document.querySelector('#querty');
+const phrase = document.querySelector('#phrase');
+const missed = 0;
+const startButton = document.querySelector('.btn__reset');
+
+startButton.addEventListener ('click', () => {
+
+
+    
+} );
