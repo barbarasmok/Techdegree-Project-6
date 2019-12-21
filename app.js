ba04b2970a3f70f0753f312b11651fe6021959a5
BarbaraSmok <barb@kpimedia.com>
2019-12-20 22:06:37 -0300

    set up the event listiner to the start variable

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const qwerty = document.querySelector('#querty');
 const phrase = document.querySelector('#phrase');
 const missed = 0;
 const startButton = document.querySelector('.btn__reset');
+const start = document.querySelector('.start');
 
 startButton.addEventListener ('click', () => {
-
-
-    
+    start.style.display = 'none';
 } );
