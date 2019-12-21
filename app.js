444488e3e02709aa4c303c2c477cc1e5981df24c
BarbaraSmok <barb@kpimedia.com>
2019-12-20 22:46:49 -0300

    set up the loop conditions

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const qwerty = document.querySelector('#querty');
-const phrases = document.querySelector('#phrase');
+const qwerty = document.querySelector('querty');
+const phrases = document.querySelector('phrase');
 const missed = 0;
-const startButton = document.querySelector('.btn__reset');
-const start = document.querySelector('.start');
+const startButton = document.querySelector('btn__reset');
+const start = document.querySelector('start');
+const finalWiner = document.querySelector('finalWinner');
+
 
 //hide the display start class once you click the start button
 startButton.addEventListener ('click', () => {
@@ -22,10 +24,19 @@ phrases = [
 
 function getRandomPhraseAsArray(arr) {
     for (var i = 0; i < arr.length; i += 1) {
-
-
+        if (qwerty.addEventListener ('click', () => {
+                qwerty[i] === phrases;
+                const winnerButton = document.createElement('button');
+                button.textContent = 'winner';
+                finalWinner.appendChild('winnerButton');    
+            });  
+        } else {
+            const loserButton = document.createElement('button');
+            button.textContent = 'try again';
+            finalWinner.appendChild('loserButton');
+            loserButton.style.display = 'none';
+        }
     }
-
 }
 
 getRandomPhraseAsArray(phrases);
